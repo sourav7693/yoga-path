7137ced41db2d084018728045b7994eb291d1db8
neelgit1010 <neel.binbrain1010@gmail.com>
2026-03-18 17:26:26 +0530

    next config update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "standalone",
+  reactStrictMode: true,  
   images: {
     remotePatterns: [
       {
